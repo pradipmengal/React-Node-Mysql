a4c983965114d9e117250c9fc52e2f1221a9e028
pradipmengal <97036198+pradipmengal@users.noreply.github.com>
2025-06-26 13:55:27 +0530

    Update script.sql

--- script.sql
+++ script.sql
@@ -4,7 +4,7 @@ CREATE DATABASE IF NOT EXISTS appdb;
 -- Use the appdb database
 USE appdb;
 
--- Create the apptb table
+-- Create the apptb table.
 CREATE TABLE `appdb`.`apptb` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
